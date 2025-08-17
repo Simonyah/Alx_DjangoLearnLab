d563e97c8313fe8e2ddf8866b72872b22b9421ff
Simonyah <163657794+Simonyah@users.noreply.github.com>
2025-08-18 03:24:32 +0900

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -2,33 +2,7 @@ from django.db.models import Q
 from django.views.generic import ListView
 from .models import Post
 
-from .views import PostSearchView
-
-from django.db.models import Q
-from django.views.generic import ListView
-from .models import Post
-
 # Search Posts
-class PostSearchView(ListView):
-    model = Post
-    template_name = 'blog/search_results.html'
-    context_object_name = 'posts'
-
-    def get_queryset(self):
-        query = self.request.GET.get('q')
-        if query:
-            return Post.objects.filter(
-                Q(title__icontains=query) |
-                Q(content__icontains=query) |
-                Q(tags__name__icontains=query)
-            ).distinct()
-        return Post.objects.none()
-
-
-urlpatterns += [
-    path('search/', PostSearchView.as_view(), name='post-search'),
-]
-
 class PostSearchView(ListView):
     model = Post
     template_name = 'blog/search_results.html'

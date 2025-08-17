4482c72b0b59b811cd63ad7c34ecad2e0f77d156
Simonyah <163657794+Simonyah@users.noreply.github.com>
2025-08-18 03:26:42 +0900

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,19 +1,44 @@
 from django.db.models import Q
-from django.views.generic import ListView
-from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.shortcuts import get_object_or_404
+from django.urls import reverse_lazy
+from .models import Post, Comment, Tag
 
-# Search Posts
-class PostSearchView(ListView):
+# Post CRUD Views
+
+class PostListView(ListView):
     model = Post
-    template_name = 'blog/search_results.html'
+    template_name = 'blog/post_list.html'
     context_object_name = 'posts'
 
-    def get_queryset(self):
-        query = self.request.GET.get('q')
-        if query:
-            return Post.objects.filter(
-                Q(title__icontains=query) |
-                Q(content__icontains=query) |
-                Q(tags__name__icontains=query)
-            ).distinct()
-        return Post.objects.none()
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content', 'tags']
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content', 'tags']
+    template_name = 'blog/post_form.html'
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy('post-list')
+    template_name = 'blog/post_confirm_delete.html'
+
+    def test_func(self):
+        post = self.get_object()
+        return self.reques

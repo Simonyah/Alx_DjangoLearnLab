ba6feed54e587e5ca3bc9e8cc17ee159207032c6
Simonyah <163657794+Simonyah@users.noreply.github.com>
2025-08-18 02:44:43 +0900

    Update urls.py

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),  # ✅ blog app routes
+    path('', include('blog.urls')),  # add this line
 ]

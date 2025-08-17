1d5d25d4a644e3abd2585746512c762686905443
Simonyah <163657794+Simonyah@users.noreply.github.com>
2025-08-18 03:02:49 +0900

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.urls import reverse
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -10,6 +12,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    # ✅ Step 6: get_absolute_url method
+    def get_absolute_url(self):
+        return reverse("post-detail", kwargs={"pk": self.pk})
 
 
 class Comment(models.Model):
